a1cb2978bc2eba481cb5ee51e33beff00284e412
BogdanGrynjuk <bgrynjuk@ukr.net>
2025-01-14 18:19:39 +0200

    Install tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import StatusLabel, { Status } from '@/app/components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
-      <h2>CRM for vendors</h2>
+      <h1 className="text-xl">Home page</h1>
+      <h2 className="text-l font-bold capitalize">app CRM for vendors</h2>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
